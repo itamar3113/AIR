d5a6a34b84a8aa1f11d1a1359b78bb7964f610fb
ronlebiush <lebiush1@gmail.com>
2025-01-10 02:12:07 +0200

    RCS

--- HW2/RCSPlanner.py
+++ HW2/RCSPlanner.py
@@ -1,4 +1,6 @@
+import heapdict
 import numpy as np
+
 
 class RCSPlanner(object):    
     def __init__(self, planning_env):
@@ -6,7 +8,10 @@ class RCSPlanner(object):
 
         # used for visualizing the expanded nodes
         # make sure that this structure will contain a list of positions (states, numpy arrays) without duplicates
-        self.expanded_nodes = [] 
+        self.expanded_nodes = []
+
+        self.fine_directions = [(0, -1), (1, 0), (0, 1), (-1, 0), (-1, -1), (-1, 1), (1, 1), (1, -1)]
+        self.coarse_directions = [(0, -2), (2, 0), (0, 2), (-2, 0), (-2, -2), (-2, 2), (2, 2), (2, -2)]
 
     def plan(self):
         '''
@@ -17,8 +22,37 @@ class RCSPlanner(object):
         plan = []
 
         # TODO: Task 4
+        COARSE = 0
+        FINE = 1
+        open_heapdict = heapdict.heapdict()
+        closed_heapdict = heapdict.heapdict()
+        open_heapdict[str(self.planning_env.start)] = node(self.planning_env.start, None, None,0) # (rank, node(state,resolution, parent))
+        while open_heapdict:
+            current_node_str ,curr_node = open_heapdict.popitem()
 
-        return np.array(plan)
+            if self.planning_env.state_validity_checker(curr_node.state):
+                if current_node_str not in closed_heapdict:
+                   
+                    if np.array_equal(curr_node.state, self.planning_env.goal):
+                        return  self.reconstruct_path(curr_node.parent)
+                    
+                    self.expanded_nodes.append(curr_node.state)# TODO:check if its the right location to add the expanded node
+
+                    for direction in self.coarse_directions:
+                        new_state = curr_node.state + direction
+                        new_rank = curr_node.rank + 1
+                        new_node = node(new_state, COARSE, curr_node,new_rank)
+                        open_heapdict[str(new_state)] = new_node
+                    closed_heapdict[current_node_str] = curr_node
+                    
+            if not np.array_equal(curr_node.state,self.planning_env.start) and curr_node.resolution == COARSE:
+                for direction in self.fine_directions:
+                    parant = curr_node.parent
+                    new_state = parant.state + direction
+                    open_heapdict[str(new_state)] =  node(new_state, FINE, parant,new_rank)
+        
+        return np.array(plan)                
+
 
     def reconstruct_path(self, node):
         '''
@@ -41,3 +75,13 @@ class RCSPlanner(object):
 
         # used for visualizing the expanded nodes
         return self.expanded_nodes
+
+class node:
+    def __init__(self, state,resolution, parent,rank):
+        self.state = state
+        self.parent = parent
+        self.resolution = resolution
+        self.rank = rank
+    
+    def __lt__(self, other):
+        return self.rank < other.rank
