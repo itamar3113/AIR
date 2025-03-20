626c2d6c829cb377e3f5e38ccfa3bcd92ba73dc8
itamar3113 <itamar3113@gmail.com>
2025-03-20 18:53:17 +0200

    task1.2

--- project/task1_1.py
+++ project/task1_1.py
@@ -8,17 +8,28 @@ from mujoco_env.sim_env import SimEnv
 workspace_x_lims = [-1.0, -0.45]
 workspace_y_lims = [-1.0, -0.45]
 """
+FACING_UP_R = [[0, -1, 0],
+               [1, 0, 0],
+               [0, 0, 1]]
+
 
 # Locate an area where both robots can reach
 # You can choose any location in the area to place the blocks
-def stack_blocks_at_position(x, y, z,blocks):
-    z=0
+def stack_blocks_at_position(x, y, z, blocks):
+    z = 0
     for block in blocks:
         z += 0.15
-        executor.pick_up("ur5e_2",block[0],block[1],block[2]+0.12) 
+        executor.pick_up("ur5e_2", block[0], block[1], block[2] + 0.12)
         executor.plan_and_move_to_xyz_facing_down("ur5e_2", [x, y, z])
-        executor.put_down("ur5e_2", x, y, z+ 0.05)
-        
+        executor.put_down("ur5e_2", x, y, z + 0.05)
+
+
+def cube_transfer(block):
+    executor.pick_up("ur5e_2", block[0], block[1], block[2] + 0.12)
+    executor.plan_and_move_to_xyz('ur5e_2', (block[0], block[1] + 0.1, block[2] + 0.22), FACING_UP_R)
+    executor.pick_up('ur5e_1', block[0], block[1] + 0.1, block[2] + 0.32)
+    executor.moveL('ur5e_1', (block[0], block[1] + 0.1, block[2] + 0.26))
+
 
 block_position = [
     [-0.7, -0.6, 0.03],
@@ -33,21 +44,11 @@ executor = MotionExecutor(env)
 # Add blocks to the world by enabling the randomize argument and setting the block position in the reset function of the SimEnv class 
 env.reset(randomize=False, block_positions=block_position)
 
-# moveJ is utilized when the robot's joints are clear to you but use carefully because there is no planning here
-move_to = [1.305356658502026, -0.7908733209856437, 1.4010098471710881, 4.102251451313659, -1.5707962412281837, -0.26543967541515895]
-executor.moveJ("ur5e_2", move_to)
 
-# executor.pick_up("ur5e_2", -0.7, -0.6, 0.15)
-
-# executor.plan_and_move_to_xyz_facing_down("ur5e_2", [-0.7, -0.7, 0.15])
-# executor.put_down("ur5e_2", -0.7, -0.7, 0.20)
 stack_blocks_at_position(-0.6, -0.7, 0.03, block_position)
 
+env.reset(randomize=False, block_positions=block_position)
 
+cube_transfer(block_position[0])
 
 executor.wait(4)
-
-
-
-
-
